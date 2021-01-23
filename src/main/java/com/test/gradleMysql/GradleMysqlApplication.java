0be83d9f356f9d86b53f2119c7f086cc70bfcfad
ula <urszulasubkowska@gmail.com>
2021-01-23 11:43:00 +0100

    bootstrap

---
+++ src/main/java/com/test/gradleMysql/GradleMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.test.gradleMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/gradleMysql/GradleMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gradleMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
